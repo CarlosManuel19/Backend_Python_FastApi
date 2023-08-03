6829026b5e89c40af2537ee5fc14b37325cbdc37
CarlosManuel19/ <cmanuel192001@gmail.com>
2023-08-02 22:14:54 -0500

    Guardar cambios

--- users.py
+++ users.py
@@ -42,6 +42,21 @@ async def user(user:User):
         return {"error":"El usuario ya existe"}
     else:
         users_list.append(user)
+        
+@app.put("/user/")
+async def user(user: User):
+    found = False
+    
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == user.id:
+            users_list[index] = user
+            found = True
+            
+    if not found:
+        return {"error":"No se ha actualizado el usuario"}
+    
+    
+    
 
     
 def search_user(id:int):
